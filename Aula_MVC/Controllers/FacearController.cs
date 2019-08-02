96a3caab3e681b800c1eb7b72129e59680f37e1c
Leonardo Sampaio dos Santos <sant0sl@outlook.com>
2019-08-01 21:49:55 -0300

    Primeira aula com criação de sites, controllers e views

---
+++ Aula_MVC/Controllers/FacearController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula_MVC.Controllers
+{
+    public class FacearController : Controller
+    {
+        // GET: Facear
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        public ActionResult Professores()
+        {
+            return View();
+        }
+
+        public ActionResult Cursos()
+        {
+            return View();
+        }
+    }
+}
